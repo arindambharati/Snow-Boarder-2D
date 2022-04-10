7934ae926fc1871af188800dd78669fb0897fe2e
arindambharati <arindambharati@gmail.com>
2022-04-10 12:44:30 +0530

    Added SoundManager, BgMusic

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    public static SoundManager Instance { get { return instance; } }
+
+    
+   // public AudioSource soundEffect;
+    public AudioSource soundMusic;
+    public SoundType[] Audio;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        Play(Sounds.BackgroundMusic);
+    }
+
+    public void Play(Sounds sound)
+    {
+        AudioClip clip = getSoundClip(sound);
+        if (clip != null){
+            soundMusic.clip = clip;
+            soundMusic.Play();
+        }
+        else{
+            Debug.LogError("Clip not found for soundtype: " + sound);
+        }
+    }
+
+    private AudioClip getSoundClip(Sounds sound)
+    {
+        SoundType audioType= Array.Find(Audio, item => item.soundType == sound);
+        if (audioType != null) return audioType.soundClip;
+        return null;
+    }
+
+    [Serializable]
+    public class SoundType
+    {
+        public Sounds soundType;
+        public AudioClip soundClip;
+    }
+
+    public enum Sounds
+    {
+        BackgroundMusic,
+        PlayerMove,
+        PlayerDeath
+    }
+}
